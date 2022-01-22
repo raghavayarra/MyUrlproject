1dcd6fcdb58a242edae979099eea94315c3b6e42
raghavayarra <53950671+raghavayarra@users.noreply.github.com>
2022-01-22 19:15:43 +0530

    Course3 Updated

--- Section3.py
+++ Section3.py
@@ -1,18 +1,50 @@
-# strings 
+# Inheritance
 
-print("hi")
-print('hi\'s a good person')
+class Animal:  
+    def speak(self):  
+        print("Animal Speaking")  
+#child class Dog inherits the base class Animal  
+class Dog(Animal):  
+    def bark(self):  
+        print("dog barking")  
+d = Dog()  
+d.bark()  
+d.speak()  
 
 
-# Concatenate
+#Multiple inheritance
 
-a="raghavendra"
-b="yarrra"
-print(a+b)
+class Calculation1:  
+    def Summation(self,a,b):  
+        return a+b;  
+class Calculation2:  
+    def Multiplication(self,a,b):  
+        return a*b;  
+class Derived(Calculation1,Calculation2):  
+    def Divide(self,a,b):  
+        return a/b;  
+d = Derived()  
+print(d.Summation(10,20))  
+print(d.Multiplication(10,20))  
+print(d.Divide(10,20))
 
 
-# int to str
+# polymorphism
+class Duck:
+    def talk(self):
+        print('Quack.. Quack..')
+class Dog:
+    def talk(self):
+        print('Bow Bow..')
+class Cat:
+    def talk(self):
+        print('Moew Moew ..')
+class Goat:
+    def talk(self):
+        print('Myaah Myaah ..')
+def f1(obj):
+    obj.talk()
 
-a=18
-b= str(a)
-print("hi is "+b)
+l=[Duck(),Cat(),Dog(),Goat()]
+for obj in l:
+    f1(obj)
